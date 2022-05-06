a793808ec01bcebc6ec703bc193da4355fa4c043
Kiranclutch1998 <poojarikiran39@gmail.com>
2022-05-06 14:07:29 +0530

    code changed for config file

--- parameters.py
+++ parameters.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 import yaml
-import logging
 
 
 def read_params(config_path):
@@ -9,11 +8,13 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
+
 def main(config_path, datasource):
     config = read_params(config_path)
     print(config)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     default_config_path = os.path.join("config", "params.yaml")
     args.add_argument("--config", default=default_config_path)
