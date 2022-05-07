ad4467b6a2f18343ba90c81d8facbdaee63a2c9a
Kiranclutch1998 <poojarikiran39@gmail.com>
2022-05-07 20:24:15 +0530

    training stage added

--- src/validation.py
+++ src/validation.py
@@ -24,8 +24,7 @@ def validationRouteClient(config_path):
     return Response("Validation successful!!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     validationRouteClient(config_path=parsed_args.config)
